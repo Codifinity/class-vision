69ec7ac59482b940cceefbc68fee13bcf9f37ac2
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2023-10-22 20:13:14 +0200

    fix hero section on mobile

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -4,14 +4,14 @@ import Button from "./Button";
 export default function HeroSection() {
   return (
     <section className="w-full py-20 flex flex-col justify-center items-center">
-      <h1 className="font-raleway-semibold text-[96px] mt-10">
+      <h1 className="font-raleway-semibold lg:text-[96px] text-6xl mt-10 ">
         Class<span className="text-blue">Vision</span>
       </h1>
-      <p className="text-[#929292] w-96 text-center">
+      <p className="text-[#929292] lg:w-96 w-64 text-sm mt-5 text-center">
         lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
         ipsumlorem ipsumlorem ipsumlorem ipsum
       </p>
-      <div className="flex flex-row justify-center items-center gap-5 mt-32">
+      <div className="flex md:flex-row flex-col justify-center items-center gap-5 md:mt-32 mt-24">
         <Button
           colorful={true}
           text={"Zaloguj się"}
