5d3f7b7fc24f732eb1a9ce66507e46055b908db7
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2023-10-23 21:00:30 +0200

    Complete login page frontend

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   colorful: boolean;
   text: string;
   optionalStyle?: string;
@@ -10,18 +10,21 @@ export default function Button({
   colorful,
   text,
   optionalStyle,
+  ...props //zeby dzialal onClick i submit formularza
 }: ButtonProps) {
   return (
     <>
       {colorful ? (
         <button
           className={`bg-gradient-to-t from-blue to-light-blue hover:from-dark-blue hover:to-blue hover:text-gray-200 active:scale-90 border-[1px] border-gray-300 transition delay-100 duration-300 ease-in-out font-raleway-semibold text-[20px] text-white px-5 py-2 ${optionalStyle} rounded-md`}
+          {...props}
         >
           {text}
         </button>
       ) : (
         <button
           className={`hover:text-gray-950 active:scale-90 border-[1px] border-[#2D2D2D] hover:border-gray-950 transition delay-100 duration-300 ease-in-out font-raleway-semibold text-[20px] text-[#2D2D2D] px-5 py-2 ${optionalStyle} rounded-md`}
+          {...props}
         >
           {text}
         </button>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,21 +3,30 @@ import Image from "next/image";
 import ClassBgImg from "../../assets/Class-bg2.svg";
 import VisionBgImg from "../../assets/Vision-bg2.svg";
 
+import Button from "../components/Button"
+
 const Page = () => {
-    return (<div className="w-full flex h-screen">
-        <div className="bg-dark-blue flex-1 flex items-center justify-center flex-col p-4">
-            <div className="text-white w-full bg-yellow-400 h-full flex items-center">
-                <div className="bg-red-100 w-1/2">
-                    <h1 className="text-5xl font-medium">Witaj!</h1>
-                    <p className="text-lg mt-2">Uzupełnij poniższe pola aby się zalogować. Podaj hasło jakie otrzymałeś/aś od szkoły. Po zalogowaniu się pierwszy raz możesz je zmienić na własne</p>
+    return (<div className="w-full flex h-screen flex-col items-center justify-center md:items-stretch  md:flex-row bg-dark-blue md:bg-white">
+        <div className="relative md:static md:bg-dark-blue md:flex-1 flex items-center justify-center flex-col p-8 sm:w-full">
+            <div className="text-white w-full h-full flex items-center">
+                <div >
+                    <h1 className="text-center md:text-left text-4xl md:text-7xl font-medium">Witaj!</h1>
+                    <p className="w-full text-md text-center md:text-left md:text-xl mt-2 md:w-2/3">Uzupełnij poniższe pola aby się zalogować. Podaj hasło jakie otrzymałeś/aś od szkoły. Po zalogowaniu się pierwszy raz możesz je zmienić na własne</p>
                 </div>
             </div>
-            <div className="bg-red-100 w-full">
+            <div className="w-full hidden md:block">
                 <Image src={ClassBgImg} alt="Class text image" width={530} height={230}/>
-                <Image src={VisionBgImg} alt="Vision text image" width={650} height={230}/>
+                <Image className="mt-6 fill-red-900 text-red-900" src={VisionBgImg} alt="Vision text image" width={650} height={230}/>
             </div>
         </div>
-        <div className="flex-1"></div>
+        <div className="md:flex-1  flex items-center justify-center ">
+            {/* FORMULARZ LOGOWANIA */}
+            <form className="max-w-2xl w-full flex flex-col gap-6 px-10" action="">
+                <input type="email" name="email" id="email" className="w-full px-4 py-3 rounded-full outline-none border-2 shadow-lg shadow-transparent  focus:shadow-dark-blue/20 focus:border-light-blue" placeholder="E-Mail"/>
+                <input type="password" name="haslo" id="haslo" className="w-full px-4 py-3 rounded-full outline-none border-2 shadow-lg shadow-transparent  focus:shadow-dark-blue/20 focus:border-light-blue" placeholder="Hasło"/>
+                <Button text="Zaloguj się" colorful type="submit" optionalStyle="rounded-full"/>
+            </form>
+        </div>
     </div>)
 }
 
