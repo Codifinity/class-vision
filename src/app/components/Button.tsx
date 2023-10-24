0ec6b8bfa2615bdf77c8fc2f32510641e2de07e2
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2023-10-24 10:39:26 +0200

    Add register modal frontend

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { cn } from "@/lib/utils";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   colorful: boolean;
   text: string;
@@ -16,14 +18,14 @@ export default function Button({
     <>
       {colorful ? (
         <button
-          className={`bg-gradient-to-t from-blue to-light-blue hover:from-dark-blue hover:to-blue hover:text-gray-200 active:scale-90 border-[1px] border-gray-300 transition delay-100 duration-300 ease-in-out font-raleway-semibold text-[20px] text-white px-5 py-2 ${optionalStyle} rounded-md`}
+          className={cn('bg-gradient-to-t from-blue to-light-blue hover:from-dark-blue hover:to-blue hover:text-gray-200 active:scale-90 border-[1px] border-gray-300 transition delay-100 duration-300 ease-in-out font-raleway-semibold text-[20px] text-white px-5 py-2 rounded-md', optionalStyle)}
           {...props}
         >
           {text}
         </button>
       ) : (
         <button
-          className={`hover:text-gray-950 active:scale-90 border-[1px] border-[#2D2D2D] hover:border-gray-950 transition delay-100 duration-300 ease-in-out font-raleway-semibold text-[20px] text-[#2D2D2D] px-5 py-2 ${optionalStyle} rounded-md`}
+          className={cn('hover:text-gray-950 active:scale-90 border-[1px] border-[#2D2D2D] hover:border-gray-950 transition delay-100 duration-300 ease-in-out font-raleway-semibold text-[20px] text-[#2D2D2D] px-5 py-2 rounded-md', optionalStyle)}
           {...props}
         >
           {text}
