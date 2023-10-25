7bf875a4250de760c62af736a02aa204a86d66c5
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2023-10-25 21:53:34 +0200

    add timetable component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Navbar from "../components/Navbar";
 import RegisterModal from "../components/RegisterModal";
 import WelcomeSection from "../components/WelcomeSection";
+import Timetable from '../components/Timetable';
 
 export default function Page() {
 
@@ -11,7 +12,12 @@ export default function Page() {
       <Navbar />
       {/* MODAL ZMIANY HASLA */}
       {/* <RegisterModal/> */}
-      <WelcomeSection />
+      <div className='w-10/12 mx-auto'>
+        <div className='flex lg:flex-row flex-col justify-between items-start my-6'>
+          <WelcomeSection />
+          <Timetable />
+        </div>
+      </div>
     </div>
   );
 }
