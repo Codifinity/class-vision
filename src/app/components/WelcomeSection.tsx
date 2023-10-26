b7cb6bff1071a7722ce16e8a1206d2d2eb5de76b
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2023-10-26 08:09:23 +0200

    add minor fix

--- src/app/components/WelcomeSection.tsx
+++ src/app/components/WelcomeSection.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <section className="lg:ml-10 border-[1px] lg:w-4/6 xl:w-[70rem] p-3 py-7 lg:px-16 px-8 border-gray-300 font-raleway-medium shadow-md md:w-full w-full rounded-lg">
+    <section className="lg:ml-10 border-[1px] lg:w-4/6 2xl:w-[70rem] p-3 py-7 lg:px-16 px-8 border-gray-300 font-raleway-medium shadow-md md:w-full w-full rounded-lg">
       <h1 className="font-raleway-bold lg:text-6xl my-3 text-2xl md:text-4xl">
         <span className="text-blue">Witaj</span>, Krzysztof
       </h1>
-      <div className="lg:text-base font-raleway-semibold text-sm md:text-xl py-2 flex lg:flex-row justify-start lg:items-center flex-col">
-        <div>
+      <div className="lg:text-base font-raleway-semibold text-sm md:text-xl py-2 flex 2xl:flex-row justify-start 2xl:items-center flex-col">
+        <div className="flex flex-row gap-2">
           Aktualna lekcja:{" "}
           <span className="text-[#339873] font-raleway-semibold mr-5">
             Matematyka
