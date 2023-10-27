4bd07a085bb6b9496668a2c721a6eb7001e8e461
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2023-10-27 19:39:10 +0200

    Start creating chat layout

--- src/app/vision-chat/[[...chat]]/page.tsx
+++ src/app/vision-chat/[[...chat]]/page.tsx
@@ -1,11 +1,15 @@
+import Navbar from '../../components/Navbar';
+
+import {
+  BsFillPencilFill,
+  BsSearch,
+  BsFillCameraVideoFill
+} from 'react-icons/bs';
+import { AiFillPhone } from 'react-icons/ai';
+import ProfilePicture from '../../components/ProfilePicture';
+import ConversationCard from '../../components/ConversationCard';
+import PinnedChat from '../../components/PinnedChat';
 import Image from 'next/image';
-import Navbar from '../components/Navbar';
-
-import { BsFillPencilFill, BsSearch } from 'react-icons/bs';
-import ProfilePicture from '../components/ProfilePicture';
-import ConversationCard from '../components/ConversationCard';
-
-// import profilePicture from './preview-pic.jpg'
 
 const Page = () => {
   return (
@@ -14,7 +18,7 @@ const Page = () => {
 
       <div className="flex h-[calc(100vh-5rem)]">
         {/* Sidebar */}
-        <div className="w-2/7 bg-light-gray px-8 py-6 flex flex-col gap-8">
+        <div className="w-1/4 bg-light-gray px-8 py-6 flex flex-col gap-8">
           {/* Logged user */}
           <div className="flex w-full items-center gap-4 px-4 py-2">
             {/* Profile pic */}
@@ -52,8 +56,17 @@ const Page = () => {
             </div>
           </div>
 
-          {/* Users list */}
+          {/* Chats */}
           <div className="scrollbar-thin scrollbar-thumb-custom-dark/40 scrollbar-track-gray-200 overflow-y-scroll">
+            {/* Pinned chats */}
+            <PinnedChat
+              src="/wycieczki-preview.jpg"
+              name="Wycieczki"
+              lastMessageUserName="Małgorzata"
+              lastMessage="O której zbiórka?"
+              lastMessageTime="16:34"
+            />
+            {/* User chats */}
             <ConversationCard
               src="/preview-pic.jpg"
               name="Nauczyciel"
@@ -119,8 +132,37 @@ ale za to zrobił to z wielką gracją"
           </div>
         </div>
 
-        {/* Chat */}
-        <div className="w-5/7 bg-white h-full"></div>
+        {/* Conversation */}
+        <div className="w-3/4 h-full bg-white flex flex-col px-8 py-6 gap-4">
+          {/* Conversation target user */}
+          <div className=" w-full flex items-center justify-between border-b-2 border-custom-dark/30 pb-4 px-4">
+            {/* Picture, name */}
+            <div className="flex items-center gap-4">
+              <ProfilePicture
+                src="/preview-pic.jpg"
+                alt="Conversation profile picture"
+              />
+              <h2 className="text-gray-500 text-4xl font-bold">Nauczyciel 1</h2>
+            </div>
+
+            {/* Upper buttons */}
+            <div className="flex items-center gap-2">
+              <button className="p-2 rounded-md hover:bg-gray-200 self-end">
+                <AiFillPhone size="2em" className="text-blue rotate-90" />
+              </button>
+
+              <button className="p-2 rounded-md hover:bg-gray-200 self-end">
+                <BsFillCameraVideoFill size="2em" className="text-blue" />
+              </button>
+            </div>
+          </div>
+
+          {/* Chatbox */}
+          <div className="bg-red-200 w-full">chat</div>
+
+          {/* Message writing box */}
+          <div className="bg-red-200 w-full"></div>
+        </div>
       </div>
     </div>
   );
