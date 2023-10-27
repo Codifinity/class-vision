92e7beb399bfa1cd722ce62490b89a1b49b01345
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2023-10-27 18:51:00 +0200

    fix dashboard frontend

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+'use client';
 
 import * as React from 'react';
 
-import Navbar from "../components/Navbar";
-import RegisterModal from "../components/RegisterModal";
-import WelcomeSection from "../components/WelcomeSection";
+import Navbar from '../components/Navbar';
+import RegisterModal from '../components/RegisterModal';
+import WelcomeSection from '../components/WelcomeSection';
 import Timetable from '../components/Timetable';
 import LastGrades from '../components/LastGrades';
 import ExamsSchedule from '../components/ExamsSchedule';
@@ -13,12 +13,12 @@ export default function Page() {
   return (
     <div className="w-full">
       <Navbar />
-        <RegisterModal/>
-      <div className='w-10/12 mx-auto'>
-        <div className='flex lg:flex-row flex-col justify-between items-start my-6'>
-          <div className='w-full lg:w-auto'>
+      <RegisterModal />
+      <div className="w-10/12 mx-auto">
+        <div className="flex lg:flex-row flex-col justify-between items-start my-6">
+          <div className="w-full lg:w-auto">
             <WelcomeSection />
-            <div className='flex lg:flex-row flex-col lg:justify-between items-start justify-center mt-4 lg:gap-10 gap-5'>
+            <div className="flex lg:flex-row flex-col lg:ml-10 lg:justify-between items-start justify-center w-full mt-4 lg:gap-10 gap-5">
               <LastGrades />
               <ExamsSchedule />
             </div>
